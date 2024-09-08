a41501e0a8b48015eb9eb3afb703fc10634cfd05
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-09-08 16:29:45 +0800

    Update to Cosmic Reach 0.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ object Constants {
 
     const val DISPLAY_NAME = "Cosmic Player Deceleration"
 
-    const val VERSION_COSMIC_REACH = "0.2.2"
+    const val VERSION_COSMIC_REACH = "0.2.3"
     const val VERSION_JAVA = "17"
 }
 
